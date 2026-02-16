0bb80bb044a36ebc72c73ce8cd1663aea668f2c5
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-02-16 19:28:36 +0200

    Bugfix (mainly  typos)
    "первый блин комом"

--- stupa_root/stupa_app/urls.py
+++ stupa_root/stupa_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.start, name="start"),
     path("game/", views.game, name="game"),
-    path("board/", views.reset_game, name="board"),
+    path("board/", views.board, name="board"),
 ]
--- stupa_root/stupa_app/views.py
+++ stupa_root/stupa_app/views.py
@@ -2,42 +2,12 @@ from django.shortcuts import render, redirect
 
 
 def start(request):
-    if request.method == "POST":
-        n = int(request.POST.get("n", 3))
-        delay = int(request.POST.get("delay", 500))
-
-        if n < MIN_DISKS or n > MAX_DISKS:
-            n = 3
-
-        init_game(request, n, delay)
-        return redirect("game")
-
-    return render(request, "hanoi/start.html")
+    return render(request, "stupa_app/start.html")
 
 
 def game(request):
-    if "pegs" not in request.session:
-        return redirect("start")
-
-    return render(
-        request,
-        "hanoi/game.html",
-        {
-            "pegs": request.session["pegs"],
-            "delay": request.session["delay"],
-        },
-    )
+    return render(request, "stupa_app/game.html")
 
 
 def board(request):
-    if "pegs" not in request.session:
-        return redirect("start")
-
-    return render(
-        request,
-        "hanoi/game.html",
-        {
-            "pegs": request.session["pegs"],
-            "delay": request.session["delay"],
-        },
-    )
+    return render(request, "stupa_app/board.html")
