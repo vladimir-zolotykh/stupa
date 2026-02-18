54ced3164071154a6b433a8ec4fa20ce85508758
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-02-18 18:35:01 +0200

    Add init_game(), edit start(), game()

--- stupa_root/stupa_app/views.py
+++ stupa_root/stupa_app/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, redirect
+from . import solve
+
+
+def init_game(request, ndisks, delay):
+    request.session["ndisks"] = ndisks
+    request.session["delay"] = delay
+    request.session["pegs"] = solve.Pegs(ndisks).to_session()
 
 
 def start(request):
+    if request.method == "POST":
+        ndisks = int(request.POST.get("ndisks", 3))
+        delay = int(request.POST.get("delay", 500))
+        init_game(request, ndisks, delay)
+        return redirect("game")
+
     return render(request, "stupa_app/start.html")
 
 
 def game(request):
-    return render(request, "stupa_app/game.html")
+    pegs = solve.from_session(request)
+    return render(request, "stupa_app/game.html", context={"pegs": pegs})
 
 
 def board(request):
