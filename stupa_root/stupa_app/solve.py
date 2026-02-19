5369acd9ae32c56c1864295d1a4ec479edd82ed5
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-02-19 20:58:28 +0200

    Add __getstate__(), transpose()
    Edit game.html

--- stupa_root/stupa_app/solve.py
+++ stupa_root/stupa_app/solve.py
@@ -3,26 +3,27 @@ import copy
 
 
 class Pegs(UserDict):
-    def __init__(self, data: dict, step: int = 0):
+    def __init__(self, data: dict, ndisks: int = 3, step: int = 0):
         super().__init__(data)
-        self.step = step
+        self.step: int = step
+        self.ndisks: int = ndisks
 
-    @classmethod
-    def from_ndisks(cls, ndisks):
-        data = {
-            "A": list(range(ndisks, 0, -1)),
-            "B": [],
-            "C": [],
+    def __getstate__(self) -> dict:
+        return {
+            "data": dict(self.data),
+            "ndisks": self.ndisks,
+            "step": self.step,
         }
-        return cls(data, 0)
+
+    def __setstate__(self, state: dict) -> None:
+        self.data = state["data"]
+        self.ndisks = state["ndisks"]
+        self.step = state["step"]
 
     @classmethod
-    def from_session(cls, request):
-        return cls(request.session["pegs"], step=request.session["step"])
-
-    def to_session(self, request):
-        request.session["pegs"] = dict(copy.deepcopy(self.data))
-        request.session["step"] = self.step
+    def from_ndisks(cls, ndisks: int):
+        data = {"A": list(range(ndisks, 0, -1)), "B": [], "C": []}
+        return cls(data, ndisks=ndisks, step=0)
 
     def move(self, from_, to):
         src, dst = self.data[from_], self.data[to]
@@ -30,7 +31,7 @@ class Pegs(UserDict):
             raise ValueError(f"Cannot place {src[-1]} on top {dst[-1]}")
         disk = src.pop()
         dst.append(disk)
-        self.nsteps += 1
+        self.step += 1
 
     def solve(self, ndisks: int, from_: str, aux: str, to: str):
         if ndisks == 0:
@@ -42,4 +43,19 @@ class Pegs(UserDict):
 
 def solve(pegs: Pegs) -> int:
     pegs.solve(len(pegs["A"]), "A", "B", "C")
-    return pegs.nsteps
+    return pegs.step
+
+
+def transpose(pegs: list[list]) -> list[list]:
+    board = []
+    npegs = len(pegs)  # number of pegs
+    ndisks = len(pegs[0])  # number of disks
+    for i in range(ndisks - 1, -1, -1):
+        row = []
+        for j in range(npegs):
+            try:
+                row.append(pegs[j][i])
+            except IndexError:
+                row.append(None)
+        board.append(row)
+    return board
--- stupa_root/stupa_app/views.py
+++ stupa_root/stupa_app/views.py
@@ -3,9 +3,11 @@ from . import solve
 
 
 def init_game(request, ndisks, delay):
+    pegs = solve.Pegs.from_ndisks(ndisks)
+    request.session["pegs"] = pegs
     request.session["ndisks"] = ndisks
     request.session["delay"] = delay
-    request.session["pegs"] = solve.Pegs(ndisks).to_session()
+    request.session.modified = True
 
 
 def start(request):
@@ -19,8 +21,16 @@ def start(request):
 
 
 def game(request):
-    pegs = solve.from_session(request)
-    return render(request, "stupa_app/game.html", context={"pegs": pegs})
+    pegs = solve.Pegs.__getstate__(request)
+    return render(
+        request,
+        "stupa_app/game.html",
+        context={
+            "pegs": solve.transpose(pegs.data),
+            "ndisks": pegs.ndisks,
+            "step": pegs.step,
+        },
+    )
 
 
 def board(request):
