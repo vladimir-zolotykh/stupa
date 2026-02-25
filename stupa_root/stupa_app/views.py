18884b57b10d784557fb6e050345470ed14d16c8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-02-25 21:24:13 +0200

    Refactor/bugfix gave view
     def game(request):
         if request.method == "POST":
    +        pegs = request.session.get("pegs")

--- stupa_root/stupa_app/views.py
+++ stupa_root/stupa_app/views.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 def init_game(request, ndisks, delay):
     pegs_start = [list(range(ndisks - 1, -1, -1)), [], []]
     # pegs = pegs_start.extend(list(solve2.solve(pegs_start, ndisks)))
-    pegs = list(solve2.solve(pegs_start, ndisks))
+    pegs = [pegs_start, *list(solve2.solve(pegs_start, ndisks))]
     request.session["pegs"] = pegs
     request.session["ndisks"] = ndisks
     # pegs = solve.Pegs.from_ndisks(ndisks)
@@ -34,22 +34,23 @@ def is_solved(pegs):
 
 def game(request):
     if request.method == "POST":
+        pegs = request.session.get("pegs")
+        pegs_car, *pegs_rest = pegs
+        request.session["pegs"] = pegs_rest
         return redirect("game")
     pegs = request.session.get("pegs")
-    pegs_car, *pegs_rest = pegs
     ndisks = request.session.get("ndisks")
     # pegs = solve.Pegs.from_session_dict(pegs) if pegs else solve.Pegs.from_ndisks()
     logger.debug("pegs: %s", pegs)
-    logger.debug("pegs_car: %s", pegs_car)
-    if not is_solved(pegs):
-        request.session["pegs"] = pegs_rest
+    # logger.debug("pegs_car: %s", pegs_car)
+    # if not is_solved(pegs):
     return render(
         request,
         "stupa_app/game.html",
         context={
             # "pegs_data": pegs.data,
             # "pegs_data": solve.transpose(list(pegs.data.values())),
-            "pegs": solve.transpose(pegs_car),
+            "pegs": solve.transpose(pegs[0]),
             "ndisks": ndisks,
             "solved": is_solved(pegs),
         },
